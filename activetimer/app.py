2047526f8ffe5e8e7d3feb79ee40443172fac568
thayes46 <thayes46@gatech.edu>
2020-07-30 14:08:56 -0400

    Establish main running structure and implement google API calls

--- activetimer/app.py
+++ activetimer/app.py
@@ -1,4 +1,13 @@
 from .timing import speedtimer
+from .display import records
 
 
 def run():
+    while 1: # it's always doing one of these
+        top_times = records.get_records()
+        while 1:  # TODO: replace 1 with foot pedal checks to start
+            # display slideshow and leaderboard
+            pass
+        while 1:  # TODO: replace 1 with foot pedal checks to end
+            # run timers
+            pass
--- activetimer/display/records.py
+++ activetimer/display/records.py
@@ -1,4 +1,11 @@
 import gspread
 
-gc = gspread.service_account(filename='service_account.json')
-sh = gc.open("Record times")
+
+def get_records():
+    gc = gspread.service_account(filename='service_account.json')
+    sh = gc.open("Record times")
+    sorted_sheet = sh.get_worksheet(1)
+    # Sorted entries are stored on Sheet2 in the g sheet
+    # print statement for example/test
+    top_times = (sorted_sheet.get('A1:C10'))
+    return top_times

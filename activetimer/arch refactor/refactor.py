31ccf494dc68aa038633b94f68f78c3afa1bcdc4
thayes46 <thayes46@gatech.edu>
2020-08-08 21:10:25 -0400

    oh boi this is a headache. Going to try using a timer to trigger the updating function that starts the thread

--- activetimer/arch refactor/refactor.py
+++ activetimer/arch refactor/refactor.py
@@ -1,5 +1,7 @@
-from PyQt5.QtCore import QThread
-from PyQt5.QtWidgets import QWidget, QApplication
+import time
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
 import sys
 
 """
@@ -11,42 +13,127 @@ grey_button = Button(14)  # GPIO pin for grey finish button
 grey_pedal = Button(20)  # GPIO pin for grey start pedal
 grey_lights = LED(15) # GPIO pin for grey flashy flashies
 """
+orange_started = False
+orange_pedal_primed = False
+orange_finished = False
+orange_initial_time = 0
+orange_final_time = 0
+orange_time = 0
+grey_started = False
+grey_pedal_primed = False
+grey_finished = False
+grey_initial_time = 0
+grey_final_time = 0
+grey_time = 0
 
-class MainCaller():
+
+class MainWindow(QMainWindow):
     def __init__(self):
-        # timing_thread = TimingThread()
-        # timing_thread.finished.connect(timing_thread.window.destroy())
-        # timing_thread.start()
+        super().__init__()
+        print("starting")
+        self.timing_widget = TimingWindow()
+        self.timing_widget.show()
+        time.sleep(1)
+        self.timing_widget.begin()
+
+    def woah_nelly(self):
+        print("woah nelly")
+        self.timing_widget.begin()
+        print("settle down now")
+        self.timing_widget.yeet()
+        self.destroy()
+
+
+def reset_lanes():
+    orange_started = False
+    orange_pedal_primed = False
+    orange_finished = False
+    orange_initial_time = 0
+    orange_final_time = 0
+    orange_time = 0
+    grey_started = False
+    grey_pedal_primed = False
+    grey_finished = False
+    grey_initial_time = 0
+    grey_final_time = 0
+    grey_time = 0
 
 
 class TimingThread(QThread):
     # Thread running timer logic
     def __init__(self):
         QThread.__init__(self)
+        reset_lanes()
         self.stop_timer = False
-        self.timing_window = TimingWindow()
-        self.timing_window.show()
 
-    def run(self):
+    def go(self, TimingWindow):
         # loop
+        print("Starting main loop")
         while not self.stop_timer:
-            # test setup for structure
-
+            time.sleep(2)
+            TimingWindow.update_orange_time(1)
+            time.sleep(2)
+            self.stop_timer = True
+        print("done with loop")
 
 
 class TimingWindow(QWidget):
-    def __init__(self):
-
     # GUI for timers
+    def __init__(self):
+        super(TimingWindow, self).__init__()
 
-class DefaultThread(QThread):
-    # Thread for slideshow
+        # setting title
+        self.grey_label = QLabel(self)
+        self.orange_label = QLabel(self)
+        self.setWindowTitle("Orange and Grey lanes")
+
+        # setting window geometry
+        # Timer takes up top half of screen
+        self.setGeometry(0, 0, 1080, 960)
+
+        # setup UI
+        self.UiComponents()
+
+    def begin(self):
+        self.update_grey_time(1)
+        time.sleep(1)
+
+    # method for widgets
+    def UiComponents(self):
+        # give labels their juice
+        self.orange_label.setGeometry(0, 0, 1080, 480)
+        self.grey_label.setGeometry(0, 480, 1080, 480)
+
+        # TODO: make style decent
+        self.orange_label.setStyleSheet("border : 16px solid orange;")
+        self.grey_label.setStyleSheet("border : 16px solid grey;")
+
+        self.orange_label.setText(str(orange_time))
+        self.grey_label.setText(str(grey_time))
+
+        self.orange_label.setFont(QFont('Arial', 25))  # TODO: make text beeg
+        self.grey_label.setFont(QFont('Arial', 25))  # TODO: make text beeg
+
+        self.orange_label.setAlignment(Qt.AlignCenter)
+        self.grey_label.setAlignment(Qt.AlignCenter)
+
+    def update_orange_time(self, orange_updated_time):
+        self.orange_label.setText(str(orange_updated_time))
+
+    def update_grey_time(self, grey_updated_time):
+        self.grey_label.setText(str(grey_updated_time))
+
+    def yeet(self):
+        self.close()
+
+
+# class DefaultThread(QThread):
+# Thread for slideshow
 
 # class DefaultWindow(QWidget):
-    # keeping here just in case
+# keeping here just in case
 
 # def Main():
 app = QApplication([])
-caller = MainCaller()
-sys.exit(app.exec_())
-
+main_window = MainWindow()
+app.exec_()
